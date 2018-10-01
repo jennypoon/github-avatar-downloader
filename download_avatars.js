c200c5f8d00b08834f20c49dcf8b80ba3f4d7929
Jenny Poon <jenny.poon@live.ca>
2018-10-01 23:28:52 +0000

    tested to see if error present

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,14 @@
 var request = require('request');
+var url = 'https://api.github.com/repos/jquery/jquery/contributors';
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
