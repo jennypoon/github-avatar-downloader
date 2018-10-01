cd05f50020b6def0699046ad50b6dbe98914719f
Jenny Poon <jenny.poon@live.ca>
2018-10-01 23:54:39 +0000

    updated file to include authorization

--- download_avatars.js
+++ download_avatars.js
@@ -1,11 +1,23 @@
 var request = require('request');
+var GITHUB_TOKEN = require('secrets')
 var url = 'https://api.github.com/repos/jquery/jquery/contributors';
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+      'Authorization': GITHUB_TOKEN
+    }
+};
 
+request(options, function(err, res, body) {
+  cb(err, body);
+});
 }
+
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
