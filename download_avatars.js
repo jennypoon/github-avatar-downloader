9746fc88216bc52b711a33d024d317f6550ea4d0
Jenny Poon <jenny.poon@live.ca>
2018-10-02 02:44:07 +0000

    add two command line arguments to pass value through bash terminal

--- download_avatars.js
+++ download_avatars.js
@@ -1,7 +1,8 @@
 var request = require('request');
 var secrets = require('./secrets.js')
 var fs = require('fs');
-
+var owner = process.argv[2];
+var repo = process.argv[3];
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
@@ -50,7 +51,7 @@ request.get(url)
 
 };
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors(owner, repo, function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
